8985637978554dca007d0e3b435379ad3e8efa9f
Ralph Sablaon <rsablaon88@gmail.com>
2019-11-09 22:28:28 -0800

    fixed typos

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-router.use("/")
+router.route("/")
 	.get(booksController.findAll)
 	.post(booksController.create);
 
